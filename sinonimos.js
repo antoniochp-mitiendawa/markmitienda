b3a71149256ae5762cfd357826b3cb5865eb8739
antoniochp-mitiendawa <antonio.chp@gmail.com>
2026-04-05 09:51:22 -0600

    Create sinonimos.js

---
+++ sinonimos.js
@@ -0,0 +1,69 @@
+module.exports = {
+    saludos: {
+        manana: [
+            "¡Buenos dias!", "¡Muy buen dia!", "¡Excelente mañana!", "¡Feliz amanecer!", 
+            "¡Que tal, buen dia!", "¡Hola! Buen dia", "¡Despierta con energia!", 
+            "¡Que tengas lindo dia", "¡Buen dia a todos!", "¡Feliz inicio de dia",
+            "¡Que tengas un excelente dia", "¡Bendecido dia", "¡Buen dia familia",
+            "¡Amanecer con todo", "¡A darle con todo este dia"
+        ],
+        tarde: [
+            "¡Buenas tardes!", "¡Excelente tarde!", "¡Feliz tarde!", "¡Disfruta la tarde!", 
+            "¡Que tal tu tarde!", "¡Hola! Buena tarde", "¡Para que aproveches la tarde", 
+            "¡Que tengas buena tarde", "¡Buena tarde a todos!", "¡Feliz resto de tarde",
+            "¡Que pases buena tarde", "¡Disculpa la hora", "¡Para amenizar la tarde",
+            "¡Tarde productiva", "¡Que tal va tu tarde"
+        ],
+        noche: [
+            "¡Buenas noches!", "¡Excelente noche!", "¡Feliz descanso!", "¡Que tal tu noche!", 
+            "¡Espero tengas linda noche!", "¡Hola! Buena noche", "¡Para cerrar bien el dia", 
+            "¡Que descanses", "¡Buena noche a todos!", "¡Feliz noche",
+            "¡Que tengas dulces sueños", "¡Hasta mañana", "¡Descansa bien",
+            "¡Ultima oportunidad del dia", "¡Para los que estan despiertos"
+        ]
+    },
+    intro: [
+        "Hola, colaboradores de",
+        "Estimados amigos de",
+        "Qué tal, grupo",
+        "Saludos cordiales a",
+        "Muy buenas a todo el equipo de",
+        "Atento aviso para",
+        "Comunicado especial para",
+        "Notificacion para",
+        "Informacion para",
+        "Aviso importante para",
+        "Compartimos con",
+        "Presentamos a",
+        "Queremos mostrar a",
+        "Tienes que ver esto",
+        "Echa un vistazo",
+        "Presta atencion",
+        "Te invitamos a ver",
+        "No te pierdas",
+        "Mira lo que tenemos",
+        "Checa esto"
+    ],
+    llamado: [
+        "Solicita el tuyo por privado",
+        "Reserva el tuyo ahora",
+        "Aprovecha esta oferta",
+        "Ordena el tuyo ya",
+        "No te quedes sin el tuyo",
+        "Cotiza sin compromiso",
+        "Precio especial por hoy",
+        "Ultimas unidades disponibles",
+        "Envios a todo mexico",
+        "Aceptamos todas las tarjetas",
+        "Pregunta por nuestras promociones",
+        "Contactanos para mas info",
+        "Disponible por tiempo limitado",
+        "Haz tu pedido hoy",
+        "Entrega inmediata",
+        "Paga contra entrega",
+        "Despacho el mismo dia",
+        "Stock disponible",
+        "Precio de lanzamiento",
+        "Oferta valida por hoy"
+    ]
+}
