f361d45eb9d6e24c573b3db2f3e887e28850d11f
antoniochp-mitiendawa <antonio.chp@gmail.com>
2026-04-05 21:36:10 -0600

    Update sinonimos.js

--- sinonimos.js
+++ sinonimos.js
@@ -1,26 +1,8 @@
-module.exports = {
+export default {
     saludos: {
-        manana: [
-            "¡Buenos dias!", "¡Muy buen dia!", "¡Excelente mañana!", "¡Feliz amanecer!", 
-            "¡Que tal, buen dia!", "¡Hola! Buen dia", "¡Despierta con energia!", 
-            "¡Que tengas lindo dia", "¡Buen dia a todos!", "¡Feliz inicio de dia",
-            "¡Que tengas un excelente dia", "¡Bendecido dia", "¡Buen dia familia",
-            "¡Amanecer con todo", "¡A darle con todo este dia"
-        ],
-        tarde: [
-            "¡Buenas tardes!", "¡Excelente tarde!", "¡Feliz tarde!", "¡Disfruta la tarde!", 
-            "¡Que tal tu tarde!", "¡Hola! Buena tarde", "¡Para que aproveches la tarde", 
-            "¡Que tengas buena tarde", "¡Buena tarde a todos!", "¡Feliz resto de tarde",
-            "¡Que pases buena tarde", "¡Disculpa la hora", "¡Para amenizar la tarde",
-            "¡Tarde productiva", "¡Que tal va tu tarde"
-        ],
-        noche: [
-            "¡Buenas noches!", "¡Excelente noche!", "¡Feliz descanso!", "¡Que tal tu noche!", 
-            "¡Espero tengas linda noche!", "¡Hola! Buena noche", "¡Para cerrar bien el dia", 
-            "¡Que descanses", "¡Buena noche a todos!", "¡Feliz noche",
-            "¡Que tengas dulces sueños", "¡Hasta mañana", "¡Descansa bien",
-            "¡Ultima oportunidad del dia", "¡Para los que estan despiertos"
-        ]
+        manana: ["¡Buenos dias!", "¡Muy buen dia!", "¡Excelente mañana!", "¡Feliz amanecer!", "¡Que tal, buen dia!", "¡Hola! Buen dia", "¡Despierta con energia!", "¡Que tengas lindo dia", "¡Buen dia a todos!", "¡Feliz inicio de dia", "¡Que tengas un excelente dia", "¡Bendecido dia", "¡Buen dia familia", "¡Amanecer con todo", "¡A darle con todo este dia"],
+        tarde: ["¡Buenas tardes!", "¡Excelente tarde!", "¡Feliz tarde!", "¡Disfruta la tarde!", "¡Que tal tu tarde!", "¡Hola! Buena tarde", "¡Para que aproveches la tarde", "¡Que tengas buena tarde", "¡Buena tarde a todos!", "¡Feliz resto de tarde", "¡Que pases buena tarde", "¡Disculpa la hora", "¡Para amenizar la tarde", "¡Tarde productiva", "¡Que tal va tu tarde"],
+        noche: ["¡Buenas noches!", "¡Excelente noche!", "¡Feliz descanso!", "¡Que tal tu noche!", "¡Espero tengas linda noche!", "¡Hola! Buena noche", "¡Para cerrar bien el dia", "¡Que descanses", "¡Buena noche a todos!", "¡Feliz noche", "¡Que tengas dulces sueños", "¡Hasta mañana", "¡Descansa bien", "¡Ultima oportunidad del dia", "¡Para los que estan despiertos"]
     },
     intro: [
         "Hola, colaboradores de",
@@ -43,27 +25,5 @@ module.exports = {
         "No te pierdas",
         "Mira lo que tenemos",
         "Checa esto"
-    ],
-    llamado: [
-        "Solicita el tuyo por privado",
-        "Reserva el tuyo ahora",
-        "Aprovecha esta oferta",
-        "Ordena el tuyo ya",
-        "No te quedes sin el tuyo",
-        "Cotiza sin compromiso",
-        "Precio especial por hoy",
-        "Ultimas unidades disponibles",
-        "Envios a todo mexico",
-        "Aceptamos todas las tarjetas",
-        "Pregunta por nuestras promociones",
-        "Contactanos para mas info",
-        "Disponible por tiempo limitado",
-        "Haz tu pedido hoy",
-        "Entrega inmediata",
-        "Paga contra entrega",
-        "Despacho el mismo dia",
-        "Stock disponible",
-        "Precio de lanzamiento",
-        "Oferta valida por hoy"
     ]
 }
